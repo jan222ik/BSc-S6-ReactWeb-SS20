c13f9406385a1aa0eee4f05ab6a1fa4721a1537e
Janik Mayr <jan.222.ik@gmail.com>
2020-04-02 17:03:56 +0200

    Assignment 5 complete

--- Assignment05/src/index.tsx
+++ Assignment05/src/index.tsx
@@ -8,7 +8,7 @@ import {colors} from "./colors";
 const MAX_DISPLAY_NUM = 5;
 const ALLOW_TOGGLE_READ = true;
 
-type Props = {
+type StyleProps = {
     theme: {
         colors: {
             bg: string,
@@ -17,86 +17,89 @@ type Props = {
             secondary: string,
             fontColor: string
         }
+    },
+    nav: {
+        isSelected: boolean
     }
 };
 
 const GlobalStyle = createGlobalStyle`
-
-    body {
-        background-color: ${(props: Props) => props.theme.colors.bg};
-        color: ${(props: Props) => props.theme.colors.fontColor};
-         -webkit-animation: fadein 2s; /* Safari, Chrome and Opera > 12.1 */
-  -moz-animation: fadein 2s; /* Firefox < 16 */
-  -ms-animation: fadein 2s; /* Internet Explorer */
-  -o-animation: fadein 2s; /* Opera < 12.1 */
-  animation: fadein 2s;
-    }
-    
-    nav {
-        width: 100%;
-        top: 0;
-        height: 4vh;
-        display: flex;
-        justify-content: flex-end;
-    
-        & > button {
-            margin-right: 1vw;
+        body {
+            background-color: ${(props: StyleProps) => props.theme.colors.bg};
+            color: ${(props: StyleProps) => props.theme.colors.fontColor};
+             -webkit-animation: fadein 2s; /* Safari, Chrome and Opera > 12.1 */
+            -moz-animation: fadein 2s; /* Firefox < 16 */
+            -ms-animation: fadein 2s; /* Internet Explorer */
+            -o-animation: fadein 2s; /* Opera < 12.1 */
+            animation: fadein 2s;
+            transition: background-color 250ms linear;
+            transition: color 250ms linear;
         }
-    }
-
-    ::-webkit-scrollbar {
-        width: 10px;
-    }
-
-    ::-webkit-scrollbar-track {
-        background: ${(props: Props) => props.theme.colors.bg};
-    }
-
-    ::-webkit-scrollbar-thumb {
-        background: ${(props: Props) => props.theme.colors.surface};
-    }
-
-    ::-webkit-scrollbar-thumb:hover {
-        background: ${(props: Props) => props.theme.colors.primary};
-    }
-
-    ul {
-        list-style-type: none;
-    }
+        
+        nav {
+            width: 100%;
+            top: 0;
+            height: 4vh;
+            display: flex;
+            justify-content: flex-end;
+        
+            & > button {
+                margin-right: 1vw;
+            }
+        }
     
-    @keyframes fadein {
-    from { opacity: 0; }
-    to   { opacity: 1; }
-}
-
-/* Firefox < 16 */
-@-moz-keyframes fadein {
-    from { opacity: 0; }
-    to   { opacity: 1; }
-}
-
-/* Safari, Chrome and Opera > 12.1 */
-@-webkit-keyframes fadein {
-    from { opacity: 0; }
-    to   { opacity: 1; }
-}
-
-/* Internet Explorer */
-@-ms-keyframes fadein {
-    from { opacity: 0; }
-    to   { opacity: 1; }
-}
-
-/* Opera < 12.1 */
-@-o-keyframes fadein {
-    from { opacity: 0; }
-    to   { opacity: 1; }
-}
+        ::-webkit-scrollbar {
+            width: 10px;
+        }
     
+        ::-webkit-scrollbar-track {
+            background: ${(props: StyleProps) => props.theme.colors.bg};
+        }
+    
+        ::-webkit-scrollbar-thumb {
+            background: ${(props: StyleProps) => props.theme.colors.surface};
+        }
+    
+        ::-webkit-scrollbar-thumb:hover {
+            background: ${(props: StyleProps) => props.theme.colors.primary};
+        }
+    
+        ul {
+            list-style-type: none;
+        }
+        
+        @keyframes fadein {
+            from { opacity: 0; }
+            to   { opacity: 1; }
+        }
+    
+        /* Firefox < 16 */
+        @-moz-keyframes fadein {
+            from { opacity: 0; }
+            to   { opacity: 1; }
+        }
+        
+        /* Safari, Chrome and Opera > 12.1 */
+        @-webkit-keyframes fadein {
+            from { opacity: 0; }
+            to   { opacity: 1; }
+        }
+        
+        /* Internet Explorer */
+        @-ms-keyframes fadein {
+            from { opacity: 0; }
+            to   { opacity: 1; }
+        }
+        
+        /* Opera < 12.1 */
+        @-o-keyframes fadein {
+            from { opacity: 0; }
+            to   { opacity: 1; }
+        }
     `;
 
 const MsgStyleBase = styled.div`
-        background: ${(props: Props) => props.theme.colors.surface};
+        background: ${(props: StyleProps) => props.theme.colors.surface};
         border-radius: 25px;
         padding-left: 25px;
         padding-right: 25px;
@@ -108,7 +111,7 @@ const MsgStyleBase = styled.div`
             padding-top: 10px;
         }
 
-        & > p {
+        & > pre {
             border-radius: 25px;
             padding: 5px 10px 10px 25px;
             margin: 0;
@@ -116,7 +119,7 @@ const MsgStyleBase = styled.div`
     `;
 
 const MsgStyleUnread = styled.div`
-        background: ${(props: Props) => lightenDarkenColor(props.theme.colors.surface, 10)};
+        background: ${(props: StyleProps) => lightenDarkenColor(props.theme.colors.surface, 10)};
         border-radius: 25px;
         padding-left: 25px;
         padding-right: 25px;
@@ -127,34 +130,97 @@ const MsgStyleUnread = styled.div`
             padding-top: 10px;
         }
 
-        & > p {
+        & > pre {
             border-radius: 25px;
             padding: 5px 10px 10px 25px;
             margin: 0;
+            white-space: pre-wrap
         }
     `;
 
 const NavButton = styled.button`
-  background: ${(props: Props) => props.theme.colors.primary};
-  border-radius: 25px;
-  border-width: 0;
-  transition-duration: 0.4s;
-  outline: none;
-  color: ${(props: Props) => lightenDarkenColor(props.theme.colors.surface, -13)};
-
-  -webkit-animation: fadein 2s; /* Safari, Chrome and Opera > 12.1 */
-  -moz-animation: fadein 2s; /* Firefox < 16 */
-  -ms-animation: fadein 2s; /* Internet Explorer */
-  -o-animation: fadein 2s; /* Opera < 12.1 */
-  animation: fadein 2s;
-
-  &:hover {
-    background: ${(props: Props) => props.theme.colors.secondary};
-  }
-`
+        background: ${(props: StyleProps) => (props.nav.isSelected) ? props.theme.colors.secondary : props.theme.colors.primary};
+        border-radius: 25px;
+        border-width: 0;
+        transition-duration: 0.4s;
+        outline: none;
+        color: ${(props: StyleProps) => lightenDarkenColor(props.theme.colors.surface, -13)};
+      
+        -webkit-animation: fadein 2s; /* Safari, Chrome and Opera > 12.1 */
+        -moz-animation: fadein 2s; /* Firefox < 16 */
+        -ms-animation: fadein 2s; /* Internet Explorer */
+        -o-animation: fadein 2s; /* Opera < 12.1 */
+        animation: fadein 2s;
+      
+        &:hover {
+          background: ${(props: StyleProps) => props.theme.colors.secondary};
+        }
+    `;
 
 const Container = styled.div`margin: 4vh 5vw 0;`;
 const Red = styled.span`color: red;`
+
+type MsgProps = { msg: Message, handleClick: () => void }
+const Msg = (props: MsgProps) => {
+    const contents = <><h3>{props.msg.msgTitle}</h3>
+        <pre>{props.msg.msgBody}</pre>
+    </>
+    return (!props.msg.msgRead) ? <MsgStyleBase onClick={props.handleClick}>{contents}</MsgStyleBase> :
+        <MsgStyleUnread onClick={props.handleClick}>{contents}</MsgStyleUnread>;
+};
+
+type ComposeSectionProps = { messages: Message[], setMessages: any, refFirstInput: any }
+const ComposeSection = (props: ComposeSectionProps) => {
+    const [composeTitle, setComposeTile] = useState("");
+    const [composeBody, setComposeBody] = useState("");
+
+    useEffect(() => {
+        props.refFirstInput.current.focus()
+    }, [props.messages]);
+    return <section>
+        <form onSubmit={(e) => {
+            props.setMessages([...props.messages, new Message(composeTitle, composeBody, false)]);
+            setComposeTile("")
+            setComposeBody("")
+            e.preventDefault();
+        }
+        }>
+            <label>Subject:</label><br/>
+            <input type="text"
+                   name="subject"
+                   ref={props.refFirstInput}
+                   onChange={(e) => setComposeTile(e.target.value)}
+                   value={composeTitle}/>
+            <br/><br/>
+            <label>Body:</label><br/>
+            <textarea type="text"
+                      name="body"
+                      onChange={(e) => setComposeBody(e.target.value)}
+                      value={composeBody}/>
+            <br/><br/>
+            <button>Submit!</button>
+        </form>
+    </section>;
+}
+type MsgSectionProps = { messages: Message[], setMessages: any, unreadCount: number }
+const MsgSection = (props: MsgSectionProps) => {
+    return <section>
+        {
+            (props.messages.length < 1) ? <span>You have no messages!</span> :
+                <>
+                    <Red>
+                        {(props.unreadCount > 0) ? "You have " + props.unreadCount + " unread messages!" : ""}
+                    </Red>
+                    <ul>{props.messages.map((m: Message) => <li key={m.uuid}><Msg msg={m} handleClick={
+                        () => {
+                            m.msgRead = !(ALLOW_TOGGLE_READ && m.msgRead);
+                            props.setMessages([...props.messages]);
+                        }
+                    }/></li>)}</ul>
+                </>
+        }
+    </section>;
+};
 
 
 const App = () => {
@@ -164,73 +230,19 @@ const App = () => {
     const [messages, setMessages] = useState(new Array<Message>())
     const unreadCount = messages.filter(it => !it.msgRead).length;
 
-    const Msg = (props: { msg: Message }) => {
-        const handleClick = () => {
-            props.msg.msgRead = !(ALLOW_TOGGLE_READ && props.msg.msgRead);
-            setMessages([...messages]);
-        }
-        const contents = <><h3>{props.msg.msgTitle}</h3><p>{props.msg.msgBody}</p></>
-
-        return (!props.msg.msgRead) ? <MsgStyleBase onClick={handleClick}>{contents}</MsgStyleBase> :
-            <MsgStyleUnread onClick={handleClick}>{contents}</MsgStyleUnread>;
-    };
-
-    const ComposeSection = () => {
-        const [composeTitle, setComposeTile] = useState("");
-        const [composeBody, setComposeBody] = useState("");
-
-        useEffect(() => {
-            refFirstInput.current.focus()
-        }, [messages]);
-        return <section>
-            <form onSubmit={(e) => {
-                setMessages([...messages, new Message(composeTitle, composeBody, false)]);
-                setComposeTile("")
-                setComposeBody("")
-                e.preventDefault();
-            }
-            }>
-                <label>Subject:</label><br/>
-                <input type="text"
-                       name="subject"
-                       ref={refFirstInput}
-                       onChange={(e) => setComposeTile(e.target.value)}
-                       value={composeTitle}/>
-                <br/><br/>
-                <label>Body:</label><br/>
-                <input type="text"
-                       name="body"
-                       onChange={(e) => setComposeBody(e.target.value)}
-                       value={composeBody}/>
-                <br/><br/>
-                <button>Submit!</button>
-            </form>
-        </section>;
-    }
-    const MsgSection = () => <section>
-        {
-            (messages.length < 1) ? <span>You have no messages!</span> :
-                <>
-                    <Red>
-                        {(unreadCount > 0) ? "You have " + unreadCount + " unread messages!" : ""}
-                    </Red>
-                    <ul>{messages.map(m => <li key={m.uuid}><Msg msg={m}/></li>)}</ul>
-                </>
-        }
-    </section>;
-    const Tab = () => isComposeTab ? <ComposeSection/> : <MsgSection/>
-
     return (
         <ThemeProvider theme={{colors: colors(isDark)}}>
             <GlobalStyle/>
             <Container>
                 <nav>
-                    <NavButton onClick={() => setTab(false)}>Your
-                        Messages <span>{(unreadCount > 0) ? "(" + ((unreadCount > MAX_DISPLAY_NUM) ? MAX_DISPLAY_NUM + "+" : unreadCount) + " new)" : ""}</span>
+                    <NavButton nav={{isSelected: !isComposeTab}} onClick={() => setTab(false)}>
+                        Your Messages
+                        <span>
+                            {(unreadCount > 0) ? " (" + ((unreadCount > MAX_DISPLAY_NUM) ? MAX_DISPLAY_NUM + "+" : unreadCount) + " new)" : ""}
+                        </span>
                     </NavButton>
-                    <NavButton onClick={() => {
-                        setTab(true)
-                    }}>Compose a Message
+                    <NavButton nav={{isSelected: isComposeTab}} onClick={() => setTab(true)}>
+                        Compose a Message
                     </NavButton>
                     <label>
                         Dark Mode:
@@ -243,14 +255,17 @@ const App = () => {
                             }}/>
                     </label>
                 </nav>
-                <Tab/>
+                {
+                    isComposeTab ?
+                        <ComposeSection messages={messages} setMessages={setMessages} refFirstInput={refFirstInput}/> :
+                        <MsgSection messages={messages} setMessages={setMessages} unreadCount={unreadCount}/>
+                }
             </Container>
         </ThemeProvider>
     );
 }
 
 ReactDOM.render(
-
-        <App/>
+    <App/>
     , document.getElementById('root')
 );
