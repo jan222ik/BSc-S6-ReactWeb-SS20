517a577cbadd442b44359bc11b9450b9238c0b6c
Janik Mayr <jan.222.ik@gmail.com>
2020-04-22 22:28:20 +0200

    Assignment 8 Update

--- Assignment08/src/apiaccessor.ts
+++ Assignment08/src/apiaccessor.ts
@@ -1,3 +1,4 @@
+import {key} from './secret'
 const GiphyAPI = {
     ENTRY_POINTS: {
         SEARCH: "https://api.giphy.com/v1/gifs/search",
@@ -5,7 +6,7 @@ const GiphyAPI = {
     },
     API_KEY: {
         PARAM: "api_key",
-        KEY:
+        KEY: key
     },
     PAGINATION: {
         OFFSET: {
@@ -25,7 +26,7 @@ type Response = { data: [], pagination: { total_count: number } }
 
 const LIMIT = 12;
 
-const urlOf = (...arr) => arr.join()
+const urlOf = (...arr) => arr.join('')
 const buildParameter = (isFirstParam: boolean, param: string, value: string) => (isFirstParam ? "?" : "&") + param + '=' + value;
 const ApiKeyAsParam = (isFirstParam: boolean = false) => buildParameter(isFirstParam, GiphyAPI.API_KEY.PARAM, GiphyAPI.API_KEY.KEY);
 const QueryAsParam = (s: string, isFirstParam: boolean = false) => buildParameter(isFirstParam, GiphyAPI.SEARCH.PARAM, s);
