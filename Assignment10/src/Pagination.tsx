2598a66439646dd1333ca7db6626de471ab3f165
Janik Mayr <jan.222.ik@gmail.com>
2020-05-06 21:05:24 +0200

    Assignment 10 Simplified index.tsx

--- Assignment10/src/Pagination.tsx
+++ Assignment10/src/Pagination.tsx
@@ -4,15 +4,18 @@ import NextIcon from '@material-ui/icons/ArrowForwardIos';
 
 export const Pagination = ({page, setPage}: { page: number, setPage: any }) => {
     const styleBtn = (disabled = false) => {
-        const col = !disabled ? "white" : "grey"
+        const col = !disabled ? "inherit" : "grey"
         return {background: "inherit", border: "none", color: col}
     };
     return (
         <div>
             <button style={styleBtn(page < 2)} id="page-prev" disabled={page < 2} onClick={() => setPage(page - 1)}>
-                <PrevIcon/></button>
+                <PrevIcon/>
+            </button>
             <span style={{fontSize: "1.5em"}} id="page-current">{page}</span>
-            <button style={styleBtn()} id="page-next" onClick={() => setPage(page + 1)}><NextIcon/></button>
+            <button style={styleBtn()} id="page-next" onClick={() => setPage(page + 1)}>
+                <NextIcon/>
+            </button>
         </div>
     )
 }
--- Assignment10/src/index.tsx
+++ Assignment10/src/index.tsx
@@ -1,15 +1,7 @@
 import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
-import {createMuiTheme, MuiThemeProvider} from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
 import {Pagination} from "./Pagination";
 import {PaginatedList} from "./PaginatedList";
-
-const darkTheme = createMuiTheme({
-    palette: {
-        type: 'dark',
-    },
-});
 
 const App = () => {
     const [page, setPage] = useState(1);
@@ -22,9 +14,6 @@ const App = () => {
 };
 
 ReactDOM.render(
-    <MuiThemeProvider theme={darkTheme}>
-        <CssBaseline/>
-        <App/>
-    </MuiThemeProvider>, document.getElementById('root')
+    <App/>, document.getElementById('root')
 );
 
